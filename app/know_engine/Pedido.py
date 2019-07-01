2c64cfaa339b00840b6811f20c213203d9149855
dirceugugelmin <dirceugugelmin@gmail.com>
2019-07-01 19:29:22 -0300

    Regra Produtos imp 1 a 7

--- app/know_engine/Pedido.py
+++ app/know_engine/Pedido.py
@@ -4,7 +4,8 @@ from ..facts.Produto import Produto
 
 class Pedido(KnowledgeEngine):
     # Regra 1
-    @Rule(Produto(tipo_de_venda='Loja'), OR(Produto(fase_do_item='Supply'), Produto(fase_do_item='Financeiro')),
+    @Rule(Produto(tipo_de_venda='Loja'),
+          OR(Produto(fase_do_item='Supply'), Produto(fase_do_item='Financeiro')),
           Produto(atrasoDU=P(lambda atrasoDU: atrasoDU > 40)))
     def at240(self):
         print('AT240')
@@ -18,16 +19,38 @@ class Pedido(KnowledgeEngine):
         print('AT230')
 
     # Regra 3
-    @Rule(Produto(tipo_de_venda='Loja'), OR(Produto(fase_do_item='Supply'), Produto(fase_do_item='Financeiro')),
+    @Rule(Produto(tipo_de_venda='Loja'),
+          OR(Produto(fase_do_item='Supply'), Produto(fase_do_item='Financeiro')),
           Produto(atrasoDU=P(lambda atrasoDU: atrasoDU < 20)),
           Produto(gatilho='-2du Novo Prazo'))
     def at215(self):
         print('AT215')
 
-#3--SE(E(
-#Tipo de Venda=Loja;OU(
-#Fase do Item=Supply;
-#Fase do Item=Financeiro);
-#Atraso em D.U<20;
-#Gatilho=-2d.u. Novo Prazo);
-# "AT215";
+    # Regra 4
+    @Rule(Produto(tipo_de_venda='Loja'),
+          OR(Produto(fase_do_item='Supply'), Produto(fase_do_item='Financeiro')),
+          AND(Produto(atrasoDU=P(lambda atrasoDU: atrasoDU > 20) & P(lambda atrasoDU: atrasoDU < 40))),
+          OR(Produto(gatilho='-2du Prazo Original'), Produto(gatilho='Prev.Embarque')))
+    def at225(self):
+        print('AT225')
+
+    # Regra 5
+    @Rule(Produto(tipo_de_venda='Loja'),
+          OR(Produto(fase_do_item='Supply'), Produto(fase_do_item='Financeiro')),
+          Produto(atrasoDU=P(lambda atrasoDU: atrasoDU < 20)))
+    def at205(self):
+        print('AT205')
+
+    # Regra 6
+    @Rule(Produto(tipo_de_venda='Loja'),
+          OR(Produto(fase_do_item='Operação Fiscal'), Produto(fase_do_item='Porta a Porta'), Produto(fase_do_item='Fob'), Produto(fase_do_item='Hub')),
+          Produto(gatilho='-2du Novo Prazo'))
+    def at210(self):
+        print('AT210')
+
+    # Regra 7
+    @Rule(Produto(tipo_de_venda='Loja'),
+          OR(Produto(fase_do_item='Operação Fiscal'), Produto(fase_do_item='Porta a Porta'), Produto(fase_do_item='Fob'), Produto(fase_do_item='Hub')),
+          Produto(gatilho='-2du Prazo Original'))
+    def at200(self):
+        print('AT200')
