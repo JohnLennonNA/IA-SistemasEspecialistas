9f9f1513f28f57834dc2b56ef222f279e4700316
dirceugugelmin <dirceugugelmin@gmail.com>
2019-07-01 22:09:40 -0300

    Regra Produtos imp 8 a 20

--- app/know_engine/Pedido.py
+++ app/know_engine/Pedido.py
@@ -3,12 +3,15 @@ from ..facts.Produto import Produto
 
 
 class Pedido(KnowledgeEngine):
+    ########
+    # LOJA #
+    ########
     # Regra 1
     @Rule(Produto(tipo_de_venda='Loja'),
           OR(Produto(fase_do_item='Supply'), Produto(fase_do_item='Financeiro')),
           Produto(atrasoDU=P(lambda atrasoDU: atrasoDU > 40)))
     def at240(self):
-        print('AT240')
+        print('AT240 - Regra 1')
 
     # Regra 2
     @Rule(Produto(tipo_de_venda='Loja'),
@@ -52,5 +55,95 @@ class Pedido(KnowledgeEngine):
     @Rule(Produto(tipo_de_venda='Loja'),
           OR(Produto(fase_do_item='Operação Fiscal'), Produto(fase_do_item='Porta a Porta'), Produto(fase_do_item='Fob'), Produto(fase_do_item='Hub')),
           Produto(gatilho='-2du Prazo Original'))
+    def at200(self):
+        print('AT200')
+
+    # Regra 8
+    @Rule(Produto(tipo_de_venda='Loja'),
+          OR(Produto(fase_do_item='Operação Fiscal'), Produto(fase_do_item='Fob')),
+          Produto(gatilho='Prev.Embarque'))
+    def at205(self):
+        print('AT205')
+
+    ###########
+    # TROCA   #
+    ###########
+    # Regra 9
+    @Rule(Produto(tipo_de_venda='Troca'),
+          OR(Produto(fase_do_item='Supply'), Produto(fase_do_item='Financeiro')),
+          Produto(gatilho='-2d.u. Novo Prazo'))
+    def at240(self):
+        print('AT240')
+
+    # Regra 10
+    @Rule(Produto(tipo_de_venda='Troca'),
+          OR(Produto(fase_do_item='Supply'), Produto(fase_do_item='Financeiro')))
+    def at235(self):
+        print('AT235')
+
+    # Regra 11 e 13 juntou, mesma regra
+    @Rule(Produto(tipo_de_venda='Troca'),
+          OR(Produto(fase_do_item='Operação Fiscal'), Produto(fase_do_item='Fob'), Produto(fase_do_item='Porta a Porta'), Produto(fase_do_item='Hub')),
+          Produto(gatilho='-2d.u. Novo Prazo'))
+    def at220(self):
+        print('AT220')
+
+    # Regra 12
+    @Rule(Produto(tipo_de_venda='Troca'),
+          OR(Produto(fase_do_item='Operação Fiscal'), Produto(fase_do_item='Fob')),
+          OR(Produto(gatilho='-2d.u. Prazo Original'), Produto(gatilho='Prev.Embarque')))
+    def at210(self):
+        print('AT210')
+
+    # Regra 13
+    # Juntou com a 11
+
+    # Regra 14
+    @Rule(Produto(tipo_de_venda='Troca'),
+          OR(Produto(fase_do_item='Porta a Porta'), Produto(fase_do_item='Hub')),
+          Produto(gatilho='-2d.u. Prazo Original'))
+    def at200(self):
+        print('AT200')
+
+    ###############
+    # ASSISTENCIA #
+    ###############
+    # Regra 15
+    @Rule(Produto(tipo_de_venda='Assistencia'),
+          OR(Produto(fase_do_item='Supply'), Produto(fase_do_item='Financeiro')),
+          Produto(gatilho='-2d.u. Novo Prazo'))
+    def at240(self):
+        print('AT240')
+
+    # Regra 16
+    @Rule(Produto(tipo_de_venda='Assistencia'),
+          OR(Produto(fase_do_item='Supply'), Produto(fase_do_item='Financeiro')),
+          Produto(gatilho='-2d.u. Prazo Original'))
+    def at235(self):
+        print('AT235')
+
+    # Regra 17
+    @Rule(Produto(tipo_de_venda='Assistencia'),
+          OR(Produto(fase_do_item='Supply'), Produto(fase_do_item='Financeiro')),
+          Produto(gatilho='Prev.Embarque'))
+    def at230(self):
+        print('AT230')
+
+    # Regra 18
+    @Rule(Produto(tipo_de_venda='Assistencia'),
+          OR(Produto(fase_do_item='Fob'), Produto(fase_do_item='Operação Fiscal')),
+          Produto(gatilho='-2d.u. Novo Prazo'))
+    def at220(self):
+        print('AT220')
+
+    # Regra 19
+    @Rule(Produto(tipo_de_venda='Assistencia'),
+          OR(Produto(fase_do_item='Fob'), Produto(fase_do_item='Operação Fiscal')))
+    def at210(self):
+        print('AT210')
+
+    # Regra 20
+    @Rule(Produto(tipo_de_venda='Assistencia'),
+          OR(Produto(fase_do_item='Porta a Porta'), Produto(fase_do_item='Hub')))
     def at200(self):
         print('AT200')
--- app/main.py
+++ app/main.py
@@ -1,11 +1,21 @@
-from app.facts.Gatilho import Gatilho
+from app.facts.Produto import Produto
 from app.know_engine.Logistica import Logistica
+from app.know_engine.Pedido import Pedido
 
-mt_log = Logistica()
-mt_log.reset()
+mt_ped = Pedido()
+mt_ped.reset()
 
-mt_log.declare(Gatilho(perfil='Natureba'))
-mt_log.declare(Gatilho(fome='Faminto'))
-mt_log.declare(Gatilho(horario='Almoço'))
+mt_ped.declare(Produto(tipo_de_venda='Loja'))
+mt_ped.declare(Produto(fase_do_item='Supply'))
+mt_ped.declare(Produto(atrasoDU=42))
 
-mt_log.run()
+mt_ped.run()
+
+#mt_log = Logistica()
+#mt_log.reset()
+
+#mt_log.declare(Produto(perfil='Natureba'))
+#mt_log.declare(Produto(fome='Faminto'))
+#mt_log.declare(Produto(horario='Almoço'))
+
+#mt_log.run()
