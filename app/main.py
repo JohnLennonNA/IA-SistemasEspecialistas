9e12656e3d55ac00e689f62a57117e36da463d2f
John Wendel <johnpgsp@gmail.com>
2019-07-06 08:30:11 -0300

    changes in projet

--- app/main.py
+++ app/main.py
@@ -6,25 +6,18 @@ from app.know_engine.Pedido import Produto
 from app.know_engine.Pedido import Logistica
 
 class Event(KnowledgeEngine):
-    @Rule(EventoFact(name='Extravio'), OR(EventoFact(name='ProdutoDisponivelColeta')))
-    def at204rg(self):
+    @Rule(AS.event << OR(EventoFact(name='Extravio'), EventoFact(name='ProdutoDisponivelColeta')))
+    def at204rg(self, event):
         mt_prdo = Logistica()
         mt_prdo.reset()
-        mt_prdo.declare(EventoFact(name=row['NomeEvento']))
+        mt_prdo.declare(EventoFact(name=event["name"]))
         mt_ped.run()
 
-    @Rule(EventoFact(name='ConfirmacaoOC'), OR(EventoFact(name='PagamentoAprovado')))
-    def at263rg(self):
+    @Rule(AS.event << OR( EventoFact(name='ConfirmacaoOC'), EventoFact(name='PagamentoAprovado')))
+    def at263rg(self, event):
         mt_ped = Produto()
-        d1 = date.fromisoformat(row['Data Prevista Entrega'])
-        d2 = date.today()
-        atraso = (d2 - d1).days
-        print(row['Desc Prod'], 'Atraso:', atraso)
         mt_ped.reset()
-        mt_ped.declare(ProductFact(tipo_de_venda=row['Tipo de Venda']))
-        mt_ped.declare(ProductFact(fase_do_item=row['Fase do Item']))
-        if atraso > 0:
-            mt_ped.declare(ProductFact(atrasoDU=atraso))
+        mt_ped.declare(EventoFact(name=event["name"]))
         mt_ped.run()
 
 
@@ -37,15 +30,12 @@ print(pedidos.columns)
 for index, row in pedidos.iterrows():
     # Calculo atraso do pedido
 
-    mt_ped.reset()
-    mt_ped.declare(ProductFact(name=row['Tipo de Venda']))
-    if atraso > 0:
-        mt_ped.declare(ProductFact(atrasoDU=atraso))
-    #    mt_ped.declare(Produto(gatilho='-2d.u. Novo Prazo'))
-    mt_ped.run()
+    mt_event = Event()
+    mt_event.reset()
+    mt_event.declare(EventoFact(name=row['NomeEvento']))
+    mt_event.run()
 
 
-#mt_log = Logistica()
 #mt_log.reset()
 
 #mt_log.declare(Produto(perfil='Natureba'))
